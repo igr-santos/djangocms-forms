b33e30fe0bfd3ec3b2fd7e66e06188b15b8020c3
Igor Santos <igr.exe@gmail.com>
2024-02-19 12:04:47 -0300

    fix: adapt texts to use on NOSSAS

--- djangocms_forms/models.py
+++ djangocms_forms/models.py
@@ -20,78 +20,106 @@ from .managers import ActiveFormManager
 
 
 class Form(models.Model):
-    name = models.CharField(_('Name'), max_length=255, db_index=True, editable=False)
+    name = models.CharField(_("Name"), max_length=255, db_index=True, editable=False)
 
     objects = models.Manager()
     active_objects = ActiveFormManager()
 
     class Meta:
-        verbose_name = _('form')
-        verbose_name_plural = _('forms')
+        verbose_name = _("form")
+        verbose_name_plural = _("forms")
 
     def __str__(self):
         return self.name
 
 
 class FormDefinition(CMSPlugin):
-    name = models.CharField(_('Form Name'), max_length=255)
+    name = models.CharField(_("Nome do Formulário"), max_length=255)
 
-    title = models.CharField(_('Title'), max_length=150, blank=True)
-    description = models.TextField(_('Description'), blank=True)
+    # title = models.CharField(_('Title'), max_length=150, blank=True)
+    # description = models.TextField(_('Description'), blank=True)
     submit_btn_txt = models.CharField(
-        _('Submit Button Text'), max_length=100, default=_('Submit'),
-        help_text=_('Text for the Submit Button. The default is \'Submit\''))
+        _("Texto do botão"),
+        max_length=100,
+        default=_("Enviar"),
+        help_text=_("Texto para o botão Enviar. O padrão é 'Enviar'"),
+    )
 
     post_submit_msg = models.TextField(
-        _('Post Submit Message'), blank=True, default=_('Thank You'),
-        help_text=_('Display this message to users after they submit your form.'))
+        _("Mensagem de sucesso"),
+        blank=True,
+        default=_("Obrigado"),
+        help_text=_(
+            "Exiba esta mensagem aos usuários após eles enviarem seu formulário."
+        ),
+    )
 
     # 'HTTP redirect after successful submission'
     success_redirect = models.BooleanField(
-        _('Redirect?'), default=False,
-        help_text=_('HTTP redirect after successful submission'))
+        _("Redirect?"),
+        default=False,
+        help_text=_("HTTP redirect after successful submission"),
+    )
     page_redirect = PageField(
-        verbose_name=_('Page URL'), blank=True, null=True,
+        verbose_name=_("Page URL"),
+        blank=True,
+        null=True,
         on_delete=models.SET_NULL,
-        help_text=_('A page has priority over an external URL'))
+        help_text=_("A page has priority over an external URL"),
+    )
     external_redirect = models.URLField(
-        _('External URL'), blank=True,
-        help_text=_('e.g. http://example.com/thank-you'))
+        _("External URL"), blank=True, help_text=_("e.g. http://example.com/thank-you")
+    )
     redirect_delay = models.PositiveIntegerField(
-        _('Redirect Delay'), blank=True, null=True,
-        help_text=_('Wait this number of milliseconds before redirecting. '
-                    '1000 milliseconds = 1 second.')
+        _("Redirect Delay"),
+        blank=True,
+        null=True,
+        help_text=_(
+            "Wait this number of milliseconds before redirecting. "
+            "1000 milliseconds = 1 second."
+        ),
     )
 
     # Email
     email_to = models.CharField(
-        _('Send form data to e-mail address'), max_length=255, blank=True,
-        help_text=_('Separate several addresses with a comma.'))
-    email_from = models.EmailField(_('Sender Email Address'), max_length=255, blank=True)
-    email_subject = models.CharField(_('Email Subject'), max_length=255, blank=True)
+        _("Send form data to e-mail address"),
+        max_length=255,
+        blank=True,
+        help_text=_("Separate several addresses with a comma."),
+    )
+    email_from = models.EmailField(
+        _("Sender Email Address"), max_length=255, blank=True
+    )
+    email_subject = models.CharField(_("Email Subject"), max_length=255, blank=True)
     email_uploaded_files = models.BooleanField(
-        _('Send uploaded files as email attachments'), default=True)
+        _("Send uploaded files as email attachments"), default=True
+    )
 
     # Save to database
     save_data = models.BooleanField(
-        _('Save to database'),  default=True,
-        help_text=_('Logs all form submissions to the database.'))
+        _("Save to database"),
+        default=True,
+        help_text=_("Logs all form submissions to the database."),
+    )
     spam_protection = models.SmallIntegerField(
-        _('Spam Protection'),
+        _("Spam Protection"),
         choices=settings.DJANGOCMS_FORMS_SPAM_PROTECTIONS,
-        default=settings.DJANGOCMS_FORMS_DEFAULT_SPAM_PROTECTION)
+        default=settings.DJANGOCMS_FORMS_DEFAULT_SPAM_PROTECTION,
+    )
 
     form_template = models.CharField(
-        _('Form Template'), max_length=150, blank=True,
+        _("Modelo do Formulário"),
+        max_length=150,
+        blank=True,
         choices=settings.DJANGOCMS_FORMS_TEMPLATES,
         default=settings.DJANGOCMS_FORMS_DEFAULT_TEMPLATE,
     )
 
-    plugin_reference = PluginReferenceField(Form, related_name='plugin')
+    plugin_reference = PluginReferenceField(Form, related_name="plugin")
 
     class Meta:
-        verbose_name_plural = _('forms')
-        verbose_name = _('form')
+        verbose_name_plural = _("formulários")
+        verbose_name = _("formulário")
 
     def __str__(self):
         return self.name
@@ -105,9 +133,10 @@ class FormDefinition(CMSPlugin):
 
     @property
     def upload_to(self):
-        return '%s-%s' % (
-            slugify(unidecode(self.name)).replace('_', '-'),
-            self.plugin_reference_id)
+        return "%s-%s" % (
+            slugify(unidecode(self.name)).replace("_", "-"),
+            self.plugin_reference_id,
+        )
 
     @property
     def use_honeypot(self):
@@ -124,31 +153,41 @@ class FormDefinition(CMSPlugin):
             field.save()
 
 
-@python_2_unicode_compatible
 class FormField(models.Model):
-    form = models.ForeignKey(FormDefinition, related_name='fields')
+    form = models.ForeignKey(
+        FormDefinition, related_name="fields", on_delete=models.CASCADE
+    )
     field_type = models.CharField(
-        _('Field Type'), max_length=100,
+        _("Tipo do campo"),
+        max_length=100,
         choices=settings.DJANGOCMS_FORMS_FIELD_TYPES,
-        default=settings.DJANGOCMS_FORMS_DEFAULT_FIELD_TYPE)
-    label = models.CharField(_('name'), max_length=255)
-    field_name = models.CharField(_('Custom Field Name'), max_length=255, blank=True)
-    placeholder_text = models.CharField(_('Placeholder Text'), blank=True, max_length=100)
-    required = models.BooleanField(_('Required'), default=True)
+        default=settings.DJANGOCMS_FORMS_DEFAULT_FIELD_TYPE,
+    )
+    label = models.CharField(_("Label"), max_length=255)
+    field_name = models.CharField(_("Nome do campo"), max_length=255, blank=True)
+    placeholder_text = models.CharField(_("Placeholder"), blank=True, max_length=100)
+    required = models.BooleanField(_("Obrigatório"), default=True)
     help_text = models.TextField(
-        _('Description'), blank=True,
-        help_text=_('A description / instructions for this field.'))
-    initial = models.CharField(_('Default Value'), max_length=255, blank=True)
+        _("Descrição / Texto de ajuda"),
+        blank=True,
+        help_text=_("Uma descrição/instruções para este campo."),
+    )
+    initial = models.CharField(_("Valor padrão"), max_length=255, blank=True)
     choice_values = models.TextField(
-        _('Choices'),  blank=True,
-        help_text=_('Enter options one per line. For "File Upload" '
-                    'field type, enter allowed filetype (e.g .pdf) one per line.'))
-    position = models.PositiveIntegerField(_('Position'), blank=True, null=True)
+        _("Opções"),
+        blank=True,
+        help_text=_(
+            'Insira uma opção por linha. Para "Upload de arquivo" '
+            "como tipo do campo, insira o tipo de arquivo permitido "
+            "(por exemplo, .pdf), um por linha."
+        ),
+    )
+    position = models.PositiveIntegerField(_("Posição"), blank=True, null=True)
 
     class Meta:
-        verbose_name_plural = _('fields')
-        verbose_name = _('field')
-        ordering = ('position', )
+        verbose_name_plural = _("campos")
+        verbose_name = _("campo")
+        ordering = ("position",)
 
     def __str__(self):
         return self.label
@@ -160,10 +199,10 @@ class FormField(models.Model):
             attrs.update(extra_attrs)
 
         attrs = {
-            'required': self.required,
-            'label': self.label if self.label else '',
-            'initial': self.initial if self.initial else None,
-            'help_text': self.help_text,
+            "required": self.required,
+            "label": self.label if self.label else "",
+            "initial": self.initial if self.initial else None,
+            "help_text": self.help_text,
         }
         return attrs
 
@@ -173,69 +212,89 @@ class FormField(models.Model):
         if extra_attrs:
             attrs.update(extra_attrs)
 
-        if (self.required and settings.DJANGOCMS_FORMS_USE_HTML5_REQUIRED
-                and 'required' not in attrs and self.field_type not in ('hidden', 'radio', )):
-            attrs['required'] = 'required'
+        if (
+            self.required
+            and settings.DJANGOCMS_FORMS_USE_HTML5_REQUIRED
+            and "required" not in attrs
+            and self.field_type
+            not in (
+                "hidden",
+                "radio",
+            )
+        ):
+            attrs["required"] = "required"
 
         if self.field_type in settings.DJANGOCMS_FORMS_FIELD_TYPES_WITH_PLACEHOLDER:
             if self.placeholder_text and not self.initial:
-                attrs.update({
-                    'placeholder': self.placeholder_text,
-                })
+                attrs.update(
+                    {
+                        "placeholder": self.placeholder_text,
+                    }
+                )
 
         css_classes = {
-            '__all__': (),
-            'text': ('textinput',),
-            'textarea': ('textarea', ),
-            'email': ('emailinput', ),
-            'number': ('integerfield', ),
-            'phone': ('telephoneinput', ),
-            'url': ('urlfield', ),
-            'checkbox': ('booleanfield',),
-            'checkbox_multiple': ('checkboxselectmultiple', ),
-            'select': ('choicefield', ),
-            'radio': ('radioselect', ),
-            'file': ('filefield', ),
-            'date': ('dateinput', ),
-            'time': ('timeinput', ),
-            'password': ('passwordinput', ),
-            'hidden': ('hiddeninput', ),
+            "__all__": (),
+            "text": ("textinput",),
+            "textarea": ("textarea",),
+            "email": ("emailinput",),
+            "number": ("integerfield",),
+            "phone": ("telephoneinput",),
+            "url": ("urlfield",),
+            "checkbox": ("booleanfield",),
+            "checkbox_multiple": ("checkboxselectmultiple",),
+            "select": ("choicefield",),
+            "radio": ("radioselect",),
+            "file": ("filefield",),
+            "date": ("dateinput",),
+            "time": ("timeinput",),
+            "password": ("passwordinput",),
+            "hidden": ("hiddeninput",),
         }
 
         css_classes.update(settings.DJANGOCMS_FORMS_WIDGET_CSS_CLASSES)
 
-        css_classes = (attrs.get('class', ''), ) + \
-            css_classes.get('__all__', ()) + \
-            css_classes.get(self.field_type, ())
+        css_classes = (
+            (attrs.get("class", ""),)
+            + css_classes.get("__all__", ())
+            + css_classes.get(self.field_type, ())
+        )
 
-        attrs['class'] = ' '.join(cls.strip() for cls in css_classes if cls.strip())
+        attrs["class"] = " ".join(cls.strip() for cls in css_classes if cls.strip())
         return attrs
 
     def get_choices(self):
         if self.choice_values:
-            regex = re.compile('[\s]*\n[\s]*')
+            regex = re.compile("[\s]*\n[\s]*")
             choices = regex.split(self.choice_values)
             return [(choice, choice) for choice in choices]
 
 
 class FormSubmission(models.Model):
     plugin = models.ForeignKey(
-        Form, verbose_name=_('Form'), editable=False, related_name='submissions')
-    creation_date = models.DateTimeField(_('Date'), auto_now=True)
+        Form,
+        verbose_name=_("Formulário"),
+        editable=False,
+        related_name="submissions",
+        on_delete=models.CASCADE,
+    )
+    creation_date = models.DateTimeField(_("Data de criação"), auto_now=True)
     created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, verbose_name=_('User'), editable=False, null=True)
-    ip = models.GenericIPAddressField(verbose_name='IP', blank=True, null=True)
-    referrer = models.CharField(_('Referrer URL'), max_length=150, blank=True)
+        settings.AUTH_USER_MODEL,
+        verbose_name=_("Usuário"),
+        editable=False,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    ip = models.GenericIPAddressField(verbose_name="IP", blank=True, null=True)
+    referrer = models.CharField(_("Referrer URL"), max_length=150, blank=True)
 
-    form_data = JSONField(_('Form Data'))
+    form_data = JSONField(_("Dados submetidos"))
 
     class Meta:
-        verbose_name_plural = _('form submissions')
-        verbose_name = _('form submission')
-        ordering = ('-creation_date', )
-        permissions = (
-            ('export_formsubmission', 'Can export Form Submission'),
-        )
+        verbose_name_plural = _("submissões de formulário")
+        verbose_name = _("submissão de formulário")
+        ordering = ("-creation_date",)
+        permissions = (("export_formsubmission", "Can export Form Submission"),)
 
     def __str__(self):
-        return u'%s' % self.plugin
+        return "%s" % self.plugin
